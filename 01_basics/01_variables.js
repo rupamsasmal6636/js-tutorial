3c67fef63e0dd1a496f939c489ed869d50e43a1a
Rupam Sasmal <55265419+rupamsasmal6636@users.noreply.github.com>
2023-11-01 15:30:48 +0000

    learn variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,11 +1,14 @@
-const accountId = 12512;
-let emailId = "rupamsasmal@gmail.com"
-var password = "Rupam@123"
-city = "Arambagh"
+const accountId = 12512; // used to store permanent value
+var password = "Rupam@123" // issue in block scope so should be avoid now
+let emailId = "rupamsasmal@gmail.com" // this is better than var
+city = "Arambagh" 
+let country;
 
 // accountId = 2; // can't change const 
 emailId= "rupamsasmal66@gmail.com"
 password= "Rupam123@"
 city= "Khanakul"
 
-console.log(accountId);
+console.log("id: " + accountId);
+
+console.table([accountId,emailId, password, city, country]);
