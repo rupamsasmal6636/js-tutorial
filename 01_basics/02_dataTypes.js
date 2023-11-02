aa53ee11f172a2ad2a2ae4feee429732b07f7e9e
Rupam Sasmal <rupam.sasmal.6636@gmail.com>
2023-11-02 12:14:34 +0530

    data types learn

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // to treat all the js code as newer version
+
+// alert(3+3); // give error in node but will work in browser
+
+
+// console.log(3+3) console.log(6) // this will give error
+
+console.log(3+3); console.log("Rupam");
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof "Rupam");
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
