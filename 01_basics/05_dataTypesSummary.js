7394778f03fbd77d05319dab181c05e2358ca597
Rupam Sasmal <rupam.sasmal.6636@gmail.com>
2023-11-02 21:05:13 +0530

    added dataType summary

---
+++ 01_basics/05_dataTypesSummary.js
@@ -0,0 +1,36 @@
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100 // number
+const scoreValue = 100.3 // number
+
+const isLoggedIn = false // boolean
+const outsideTemp = null  // object
+let userEmail; // undefined
+
+const id = Symbol('123') // symbol
+const anotherId = Symbol('123') // symbol
+
+console.log(id === anotherId); // false
+
+const bigNumber = 3456543576654356754n // bigint
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];  // Array object
+
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof myFunction);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
